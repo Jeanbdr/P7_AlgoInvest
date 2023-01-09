315f84764444a75083ec70a7f9d8667494da7d05
Dartois Jb <drts.alexis@gmail.com>
2023-01-09 20:02:35 +0100

    brutforce finalize and clean

--- bruteforce.py
+++ bruteforce.py
@@ -1,7 +1,3 @@
-"""LA Y'AURA DES TRUCS POUR BRUTALISER DE FORCE 
-à base de for in range  addition tout ca si ca depasse 500 ca compte pas
-on append le tout dans une liste on tri et hop magie la force est brutaliser"""
-
 import csv
 import itertools
 import time
@@ -25,31 +21,37 @@ def search_csv():
     return stocks
 
 
-stocks = search_csv()
-
-
 def create_combination(budget):
-    start = time.time()
     possible_combinations = []
     for actions_number in range(1, len(stocks) + 1):
         for combination in itertools.combinations(stocks, actions_number):
             combination_costs = [action[1] for action in combination]
             if sum(combination_costs) <= budget:
                 possible_combinations.append(combination)
-    end = time.time()
-    print("Execution time :", (end - start) * 10**3, "ms")
     return possible_combinations
 
 
-possible_combinations = create_combination(budget=500)
-print(len(possible_combinations))
-
-
 def find_best_option():
+    combinations_benefits = []
+    final_result = []
     for combination in possible_combinations:
-        print(combination)
         benefits = sum(action[3] for action in combination)
-        print(benefits)
+        combinations_benefits.append(benefits)
+    for total_roi, action_package in zip(combinations_benefits, possible_combinations):
+        final_result.append((total_roi, action_package))
+    final_result.sort(reverse=True)
+    print(
+        f"\nFYI : Each action contain : name, coast, benefits (in %) and calculated roi \n"
+    )
+    print(
+        f"Here is the best buying option to maximize your profit : {final_result[0][1]}\n"
+    )
+    print(f"This combination will bring you a profit of : {final_result[0][0]}€\n\n")
 
 
+start = time.time()
+stocks = search_csv()
+possible_combinations = create_combination(budget=500)
 find_best_option()
+end = time.time()
+print("This script took :", (end - start) * 10**3, "ms to give you this result.")
